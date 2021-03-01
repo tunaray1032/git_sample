0fcd39bcd8191c82d6858fb20abde4c91f470b53
Pan shiyun <shiyunpan16@gmail.com>
2021-03-01 15:17:50 +0900

    camera

--- master.py
+++ master.py
@@ -5,5 +5,24 @@ Created on Mon Mar  1 14:42:47 2021
 
 @author: panshiyun
 """
+import cv2  
+import numpy
+import matplotlib.pyplot as plot
+
+cap = cv2.VideoCapture(0)
+
+
+#while(1):
+    # get a frame
+   # ret, frame = cap.read()
+    # show a frame
+   # cv2.imshow("capture", frame)
+   # if cv2.waitKey(1) & 0xFF == ord('q'):
+#        break
+
+
+cap.release()
+cv2.destroyAllWindows() 
 
 print("Hellow")
+
